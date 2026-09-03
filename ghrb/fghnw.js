018281b4d7370fdf895767011268ca734cbde5a2
zipeducation <zipproject0@gmail.com>
2026-09-03 18:26:37 -0500

    Update fghnw.js

--- ghrb/fghnw.js
+++ ghrb/fghnw.js
@@ -12,8 +12,7 @@ function fetgh() {
     })
     .catch(error => {
       console.error('Fetch error: ', error, ' :(');
-      document.body.innerHTML = `<p style="margin: 0; font-family: Helvetica;">Failed to fetch the GHub data  :(<br>Report a problem:</p>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScbnf5yjlLtawLyMVYepb8VwwCwgtV4fABgQOi0eBD3_E2npQ/viewform?embedded=true" width="640" height="425" style="border: 2px solid black; border-radius: 5px;">Loading…</iframe>`;
+      document.body.innerHTML = `<p style="margin: 0; font-family: Helvetica;">Failed to fetch the zGames data :(</p>`;
     });
 }
 fetgh();
